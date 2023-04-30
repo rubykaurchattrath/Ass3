a00de43bfaeb0ffa619c36f2144115d204232802
rubykaurchattrath <122198061+rubykaurchattrath@users.noreply.github.com>
2023-04-29 23:20:19 -0700

    Update creature.cpp

--- creature.cpp
+++ creature.cpp
@@ -3,116 +3,111 @@
 
 #include "creature.h"
 
-using namespace std;
 
-ostream &operator<<(ostream &Out, const Creature &Creature) {
-  Out << "C(" << Creature.Row << "," << Creature.Col << ")";
+
+//prints out the creatures location coordinates
+std::ostream &operator<<(std::ostream &Out, const Creature &Creature) {
+  Out << "(" << Creature.Row << "," << Creature.Col << ")" << endl;
   return Out;
 }
 
-// constructor
+//constructor
 Creature::Creature(int Row, int Col) : Row(Row), Col(Col) {
   this->Row = Row;
   this->Col = Col;
 }
 
-// check if creature is at maze exit
-// return true if at exit, false otherwise
+//if its at the exit colum and row
+// returns true, if not false
 bool Creature::atExit(const Maze &Maze) const {
-  if (Row == Maze.getExitRow() && Col == Maze.getExitColumn()) {
+  if (Maze.getExitColumn() == Col && Maze.getExitRow() == Row) {
     return true;
   }
   return false;
 }
 
-// solve maze recursively using depth first search
-// param: Maze the maze object
-// return a string representation of solution path
-string Creature::solve(Maze &Maze) {
-  string Path;
 
-  // base case
+//needs maze to solve, will return a string 
+string Creature::solve(Maze &Maze) {
+
+  // if creature spawns on wall, throws invalid argument
+  if (!Maze.isClear(Row, Col)) {
+    throw invalid_argument("Invalid Creature Location");
+  }
+
+  //path with direction thats being returned.
+  string Path;  
+
+  //base case 
+  //checks if creature is atExit already
   if (atExit(Maze)) {
     Maze.markAsPath(Row,Col);
     return "";
+
+  }
+  Maze.markAsPath(Row,Col);  // Mark the current spot as part of the path
+
+
+  // check all four directions
+  // north
+  if (Maze.isClear(Row-1, Col)) {  
+    Path = goNorth(Maze);
+    if (atExit(Maze)) {
+      return Path;
+    }
+    Row++;  // Backtrack
   } 
 
-  Maze.markAsVisited(Row, Col);
-
-  // TRY NORTH
-  if (Maze.isClear(Row-1, Col)) {
-    Path = goNorth(Maze) + solve(Maze);
-    if (!Path.empty()) {
-      Maze.markAsPath(Row,Col);
+  //east
+  if (Maze.isClear(Row, Col+1)) {  
+    Path = goEast(Maze); 
+    if (atExit(Maze)) {
       return Path;
     }
-    Maze.markAsVisited(Row, Col);
+    Col--; // Backtrack
   } 
 
-  // TRY EAST
-  if (Maze.isClear(Row, Col+1)) {
-    Path = goEast(Maze) + solve(Maze);
-    if (!Path.empty()) {
-      Maze.markAsPath(Row,Col);
-      return Path;
-    }  
-    Maze.markAsVisited(Row, Col);
-  }
-
-  // TRY SOUTH
-  if (Maze.isClear(Row+1, Col)) {
-    Path = goSouth(Maze) + solve(Maze);
-    if (!Path.empty()) {
-      Maze.markAsPath(Row,Col);
+  //south
+  if (Maze.isClear(Row+1, Col)) {  
+    Path = goSouth(Maze);
+    if (atExit(Maze)) {
       return Path;
     }
-    Maze.markAsVisited(Row, Col);
-  }
+    Row--; // Backtrack
+  }   
 
-  // TRY WEST
-  if (Maze.isClear(Row, Col-1)) {
-    Path = goWest(Maze) + solve(Maze);
-    if (!Path.empty()) {
-      Maze.markAsPath(Row,Col);
+  //west
+  if (Maze.isClear(Row, Col-1)) {  
+    Path = goWest(Maze);
+    if (atExit(Maze)) {
       return Path;
     }
-    Maze.markAsVisited(Row, Col);
-  }
+    Col++; // Backtrack 
+  } 
 
+  // if none of the paths lead to the exit
+  // mark this spot as visited
   Maze.markAsVisited(Row, Col);
-  Maze.markAsPath(Row, Col);
   return "";
 }
 
-// move creature one cell to north and return direction
-// param: Maze the maze object
-// return: strign represenation of direction moved "N"
+
 string Creature::goNorth(Maze &Maze) {
-  Row--;
-  return "N" + solve(Maze);
+  Row = Row -1;
+  return "N" + solve(Maze); 
 }
 
-// move creature one cell to west and return direction
-// param: Maze the maze object
-// return: strign represenation of direction moved "W"
-string Creature::goWest(Maze &Maze) {
-  Col--;
-  return "W" + solve(Maze);
-
-}
-
-// move creature one cell to east and return direction
-// param: Maze the maze object
-// return: strign represenation of direction moved "E"
 string Creature::goEast(Maze &Maze) {
-  Col++; 
-  return "E" + solve(Maze);
+  Col = Col + 1;
+  return "E" + solve (Maze);
 }
 
-// move creature one cell to south and return direction
-// param: Maze the maze object
-// return: strign represenation of direction moved "S"
 string Creature::goSouth(Maze &Maze) {
-  Row++;
-  return "S" + solve(Maze);
-} 
+  Row = Row + 1; 
+  return "S" + solve(Maze); 
+}
+
+string Creature::goWest(Maze &Maze) {
+  Col = Col - 1;
+  return "W" + solve(Maze);
+}
